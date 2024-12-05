e6f7933c9f1baddebef5b11e82a963e0c590c69e
engAhmedSami <xotornado@gmail.com>
2024-12-05 18:00:10 +0200

    "Updated Arabic and English localization files, removed LanguageProvider and Consumer from main.dart, and simplified MaterialApp configuration."

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'package:blood_bank/feature/provider/language_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:blood_bank/feature/splash/presentation/views/splash_view.dart';
 import 'package:blood_bank/localization/generated/l10n.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => LanguageProvider()..loadLocale(),
-      child: const BloodBank(),
-    ),
-  );
+  runApp(const BloodBank());
 }
 
 class BloodBank extends StatelessWidget {
@@ -19,22 +12,17 @@ class BloodBank extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<LanguageProvider>(
-      builder: (context, languageProvider, child) {
-        return MaterialApp(
-          locale: languageProvider.locale,
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          theme: ThemeData(fontFamily: 'MPLUSRounded1c'),
-          debugShowCheckedModeBanner: false,
-          home: const SplashView(),
-        );
-      },
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      theme: ThemeData(fontFamily: 'MPLUSRounded1c'),
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
     );
   }
 }
