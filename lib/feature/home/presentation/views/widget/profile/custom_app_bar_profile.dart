727f54b5b47489f9443b25efe940c59936f43338
engAhmedSami <xotornado@gmail.com>
2024-12-11 16:56:31 +0200

    `Added tutorial coach mark for CustomProfileAppBar widget`

--- lib/feature/home/presentation/views/widget/profile/custom_app_bar_profile.dart
+++ lib/feature/home/presentation/views/widget/profile/custom_app_bar_profile.dart
@@ -1,10 +1,109 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+// import 'package:blood_bank/core/utils/assets_images.dart';
+// import 'package:blood_bank/core/utils/page_rout_builder.dart';
+// import 'package:blood_bank/feature/home/presentation/views/widget/profile/editi_user_info.dart';
+
+// class CustomProfileAppBar extends StatelessWidget
+//     implements PreferredSizeWidget {
+//   final String name;
+//   final String? photoUrl;
+//   final String userState;
+
+//   const CustomProfileAppBar({
+//     super.key,
+//     required this.name,
+//     this.photoUrl,
+//     required this.userState,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         SvgPicture.asset(
+//           Assets.imagesAppBar,
+//           fit: BoxFit.cover,
+//           width: MediaQuery.of(context).size.width,
+//           height: preferredSize.height,
+//         ),
+//         Positioned(
+//           top: 30,
+//           left: 10,
+//           child: IconButton(
+//             icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+//             onPressed: () {},
+//           ),
+//         ),
+//         Positioned(
+//           top: 30,
+//           right: 10,
+//           child: IconButton(
+//             icon: SvgPicture.asset(Assets.imagesChangeuserinfo),
+//             onPressed: () {
+//               Navigator.of(context).push(
+//                 buildPageRoute(
+//                   const UserProfilePage(),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//         Positioned(
+//           top: 20,
+//           left: MediaQuery.of(context).size.width / 2.1 - 40,
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               CircleAvatar(
+//                 radius: 40,
+//                 backgroundImage: photoUrl != null && photoUrl!.isNotEmpty
+//                     ? NetworkImage(photoUrl!)
+//                     : null,
+//                 backgroundColor: Colors.white,
+//                 child: photoUrl == null || photoUrl!.isEmpty
+//                     ? const Icon(
+//                         Icons.person,
+//                         size: 40,
+//                         color: Colors.grey,
+//                       )
+//                     : null,
+//               ),
+//               const SizedBox(height: 8),
+//               Text(
+//                 name,
+//                 style: const TextStyle(
+//                   fontSize: 20,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//               Text(
+//                 'State: $userState',
+//                 style: const TextStyle(
+//                   fontSize: 14,
+//                   color: Colors.white70,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+
+//   @override
+//   Size get preferredSize => const Size.fromHeight(220); // ارتفاع AppBar
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:blood_bank/core/utils/assets_images.dart';
 import 'package:blood_bank/core/utils/page_rout_builder.dart';
 import 'package:blood_bank/feature/home/presentation/views/widget/profile/editi_user_info.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CustomProfileAppBar extends StatelessWidget
+class CustomProfileAppBar extends StatefulWidget
     implements PreferredSizeWidget {
   final String name;
   final String? photoUrl;
@@ -18,6 +117,84 @@ class CustomProfileAppBar extends StatelessWidget
   });
 
   @override
+  State<CustomProfileAppBar> createState() => _CustomProfileAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(220); // ارتفاع AppBar
+}
+
+class _CustomProfileAppBarState extends State<CustomProfileAppBar> {
+  final GlobalKey _editButtonKey = GlobalKey(); // مفتاح التركيز على الزر
+  late TutorialCoachMark tutorialCoachMark;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfTutorialNeeded();
+  }
+
+  Future<void> _checkIfTutorialNeeded() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isFirstTime = prefs.getBool('isFirstTimeCustomProfileAppBar') ?? true;
+
+    if (isFirstTime) {
+      _showTutorial();
+      await prefs.setBool('isFirstTimeCustomProfileAppBar', false);
+    }
+  }
+
+  void _showTutorial() {
+    tutorialCoachMark = TutorialCoachMark(
+      targets: _createTargets(), // إنشاء أهداف التوجيه
+      textSkip: "SKIP",
+      hideSkip: false,
+      onFinish: () {
+        debugPrint("Tutorial finished");
+      },
+      onClickTarget: (target) {
+        debugPrint("Clicked on target: ${target.keyTarget}");
+      },
+    )..show(
+        context: context,
+      );
+  }
+
+  List<TargetFocus> _createTargets() {
+    return [
+      TargetFocus(
+        keyTarget: _editButtonKey, // الزر الذي نريد التركيز عليه
+        contents: [
+          TargetContent(
+            align: ContentAlign.bottom,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text(
+                  "Edit User Info",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Tap here to update your profile information.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+        shape: ShapeLightFocus.Circle, // شكل التركيز دائري
+      ),
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
@@ -25,7 +202,7 @@ class CustomProfileAppBar extends StatelessWidget
           Assets.imagesAppBar,
           fit: BoxFit.cover,
           width: MediaQuery.of(context).size.width,
-          height: preferredSize.height,
+          height: widget.preferredSize.height,
         ),
         Positioned(
           top: 30,
@@ -39,6 +216,7 @@ class CustomProfileAppBar extends StatelessWidget
           top: 30,
           right: 10,
           child: IconButton(
+            key: _editButtonKey, // ربط المفتاح بالزر
             icon: SvgPicture.asset(Assets.imagesChangeuserinfo),
             onPressed: () {
               Navigator.of(context).push(
@@ -57,11 +235,12 @@ class CustomProfileAppBar extends StatelessWidget
             children: [
               CircleAvatar(
                 radius: 40,
-                backgroundImage: photoUrl != null && photoUrl!.isNotEmpty
-                    ? NetworkImage(photoUrl!)
-                    : null,
+                backgroundImage:
+                    widget.photoUrl != null && widget.photoUrl!.isNotEmpty
+                        ? NetworkImage(widget.photoUrl!)
+                        : null,
                 backgroundColor: Colors.white,
-                child: photoUrl == null || photoUrl!.isEmpty
+                child: widget.photoUrl == null || widget.photoUrl!.isEmpty
                     ? const Icon(
                         Icons.person,
                         size: 40,
@@ -71,7 +250,7 @@ class CustomProfileAppBar extends StatelessWidget
               ),
               const SizedBox(height: 8),
               Text(
-                name,
+                widget.name,
                 style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -79,7 +258,7 @@ class CustomProfileAppBar extends StatelessWidget
                 ),
               ),
               Text(
-                'State: $userState',
+                'State: ${widget.userState}',
                 style: const TextStyle(
                   fontSize: 14,
                   color: Colors.white70,
@@ -91,7 +270,4 @@ class CustomProfileAppBar extends StatelessWidget
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(220); // ارتفاع AppBar
 }
