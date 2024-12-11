f2fefaabcc0ac46b7ae449687e569c16dd2ddf71
engAhmedSami <xotornado@gmail.com>
2024-12-11 17:05:29 +0200

    `Added tutorial feature to CustomProfileAppBar widget`

--- lib/feature/home/presentation/views/widget/profile/custom_app_bar_profile.dart
+++ lib/feature/home/presentation/views/widget/profile/custom_app_bar_profile.dart
@@ -1,100 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
-// import 'package:blood_bank/core/utils/assets_images.dart';
-// import 'package:blood_bank/core/utils/page_rout_builder.dart';
-// import 'package:blood_bank/feature/home/presentation/views/widget/profile/editi_user_info.dart';
-
-// class CustomProfileAppBar extends StatelessWidget
-//     implements PreferredSizeWidget {
-//   final String name;
-//   final String? photoUrl;
-//   final String userState;
-
-//   const CustomProfileAppBar({
-//     super.key,
-//     required this.name,
-//     this.photoUrl,
-//     required this.userState,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: [
-//         SvgPicture.asset(
-//           Assets.imagesAppBar,
-//           fit: BoxFit.cover,
-//           width: MediaQuery.of(context).size.width,
-//           height: preferredSize.height,
-//         ),
-//         Positioned(
-//           top: 30,
-//           left: 10,
-//           child: IconButton(
-//             icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-//             onPressed: () {},
-//           ),
-//         ),
-//         Positioned(
-//           top: 30,
-//           right: 10,
-//           child: IconButton(
-//             icon: SvgPicture.asset(Assets.imagesChangeuserinfo),
-//             onPressed: () {
-//               Navigator.of(context).push(
-//                 buildPageRoute(
-//                   const UserProfilePage(),
-//                 ),
-//               );
-//             },
-//           ),
-//         ),
-//         Positioned(
-//           top: 20,
-//           left: MediaQuery.of(context).size.width / 2.1 - 40,
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               CircleAvatar(
-//                 radius: 40,
-//                 backgroundImage: photoUrl != null && photoUrl!.isNotEmpty
-//                     ? NetworkImage(photoUrl!)
-//                     : null,
-//                 backgroundColor: Colors.white,
-//                 child: photoUrl == null || photoUrl!.isEmpty
-//                     ? const Icon(
-//                         Icons.person,
-//                         size: 40,
-//                         color: Colors.grey,
-//                       )
-//                     : null,
-//               ),
-//               const SizedBox(height: 8),
-//               Text(
-//                 name,
-//                 style: const TextStyle(
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.bold,
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               Text(
-//                 'State: $userState',
-//                 style: const TextStyle(
-//                   fontSize: 14,
-//                   color: Colors.white70,
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-
-//   @override
-//   Size get preferredSize => const Size.fromHeight(220); // ارتفاع AppBar
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:blood_bank/core/utils/assets_images.dart';
@@ -102,6 +5,7 @@ import 'package:blood_bank/core/utils/page_rout_builder.dart';
 import 'package:blood_bank/feature/home/presentation/views/widget/profile/editi_user_info.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CustomProfileAppBar extends StatefulWidget
     implements PreferredSizeWidget {
@@ -135,11 +39,17 @@ class _CustomProfileAppBarState extends State<CustomProfileAppBar> {
 
   Future<void> _checkIfTutorialNeeded() async {
     final prefs = await SharedPreferences.getInstance();
-    bool isFirstTime = prefs.getBool('isFirstTimeCustomProfileAppBar') ?? true;
+    final user = FirebaseAuth.instance.currentUser;
 
-    if (isFirstTime) {
-      _showTutorial();
-      await prefs.setBool('isFirstTimeCustomProfileAppBar', false);
+    if (user != null) {
+      // استخدم uid كمفتاح للتحقق من حالة التعليمات لهذا المستخدم
+      final tutorialKey = '${user.uid}_isFirstTimeCustomProfileAppBar';
+      bool isFirstTime = prefs.getBool(tutorialKey) ?? true;
+
+      if (isFirstTime) {
+        _showTutorial();
+        await prefs.setBool(tutorialKey, false); // حفظ الحالة للمستخدم
+      }
     }
   }
 
@@ -154,9 +64,7 @@ class _CustomProfileAppBarState extends State<CustomProfileAppBar> {
       onClickTarget: (target) {
         debugPrint("Clicked on target: ${target.keyTarget}");
       },
-    )..show(
-        context: context,
-      );
+    )..show(context: context);
   }
 
   List<TargetFocus> _createTargets() {
@@ -166,9 +74,9 @@ class _CustomProfileAppBarState extends State<CustomProfileAppBar> {
         contents: [
           TargetContent(
             align: ContentAlign.bottom,
-            child: Column(
+            child: const Column(
               mainAxisSize: MainAxisSize.min,
-              children: const [
+              children: [
                 Text(
                   "Edit User Info",
                   style: TextStyle(
