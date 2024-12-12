b7144f10aaa436107b612e2c991efd1e89f2f1d6
engAhmedSami <xotornado@gmail.com>
2024-12-12 20:35:01 +0200

    need

---
+++ lib/feature/home/presentation/views/widget/need/need_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NeedView extends StatelessWidget {
+  const NeedView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
