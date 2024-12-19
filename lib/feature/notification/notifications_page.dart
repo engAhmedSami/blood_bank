f054c7fe7fc45254390d063a216195fe5bc54725
engAhmedSami <xotornado@gmail.com>
2024-12-20 00:10:44 +0200

    `Removed color and icon customization for notifications based on notification type`

--- lib/feature/notification/notifications_page.dart
+++ lib/feature/notification/notifications_page.dart
@@ -19,23 +19,12 @@ class NotificationsPage extends StatelessWidget {
         itemBuilder: (context, index) {
           final notification = notifications[index];
 
-          // تخصيص الألوان بناءً على نوع الإشعار (يمكن تخصيصه بناءً على بيانات من الـ notification)
-          Color bgColor = Colors.white; // لون الخلفية الافتراضي
-          IconData icon = Icons.notifications; // أيقونة الإشعار الافتراضية
-
-          if (notification['isImportant'] == true) {
-            bgColor = Colors.yellow.shade100; // لون الخلفية لإشعار مهم
-            icon =
-                Icons.warning_amber_outlined; // أيقونة التحذير للإشعارات المهمة
-          }
-
           return Card(
             margin: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-            color: bgColor, // تخصيص اللون الخلفي
             elevation: 5, // إضافة ظل لزيادة التأثير الجمالي
             child: ListTile(
               leading: Icon(
-                icon,
+                Icons.notifications,
                 color: Colors.orange, // لون الأيقونة
               ),
               title: Text(
