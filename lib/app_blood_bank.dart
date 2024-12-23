0f021ab6affb46209843647e4e039f89d175d10b
Basel Embaby <engbasel2025@gmail.com>
2024-12-23 17:16:14 +0200

    `Removed UserStatusCubit and added NotificationService`

--- lib/app_blood_bank.dart
+++ lib/app_blood_bank.dart
@@ -1,6 +1,6 @@
-import 'package:blood_bank/feature/UserStatusCubit.dart';
 import 'package:blood_bank/feature/localization/app_localizations.dart';
 import 'package:blood_bank/feature/localization/cubit/locale_cubit.dart';
+import 'package:blood_bank/feature/notification/notification_service.dart';
 import 'package:blood_bank/feature/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,16 +16,11 @@ class BloodBank extends StatelessWidget {
         BlocProvider(
           create: (context) => LocaleCubit()..getSavedLanguage(),
         ),
-        BlocProvider(
-          create: (context) => UserStatusCubit(),
-        ),
       ],
       child: BlocBuilder<LocaleCubit, ChangeLocaleState>(
         builder: (context, state) {
           return MaterialApp(
-            theme: ThemeData(
-              fontFamily: 'iwanzaza',
-            ),
+            theme: ThemeData(fontFamily: 'iwanzaza'),
             locale: state.locale,
             supportedLocales: const [Locale('en'), Locale('ar')],
             localizationsDelegates: const [
@@ -53,28 +48,6 @@ class BloodBank extends StatelessWidget {
   }
 }
 
-class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: CircularProgressIndicator()));
-  }
-}
-
-class UserBlockedScreen extends StatelessWidget {
-  const UserBlockedScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: const [Center(child: Text('UserBlockedScreen'))],
-      ),
-    );
-  }
-}
-
 class SplashInitializer extends StatefulWidget {
   const SplashInitializer({super.key});
 
@@ -86,26 +59,15 @@ class _SplashInitializerState extends State<SplashInitializer> {
   @override
   void initState() {
     super.initState();
-    _initializeApp();
+    _initializeNotifications();
   }
 
-  Future<void> _initializeApp() async {}
+  Future<void> _initializeNotifications() async {
+    await NotificationService.instance.initialize(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<UserStatusCubit, String?>(
-      builder: (context, status) {
-        if (status == null) {
-          // لا يوجد مستخدم أو خطأ في التحميل -> الانتقال إلى SplashView
-          return const SplashView();
-        } else if (status == 'block') {
-          // المستخدم محظور
-          return const UserBlockedScreen();
-        } else {
-          // المستخدم مسموح له بالدخول
-          return const SplashView();
-        }
-      },
-    );
+    return SplashView();
   }
 }
