d687376d3b849f11671c0cd54862b771aad8f76d
engAhmedSami <xotornado@gmail.com>
2024-12-24 18:25:22 +0200

    `Update import path for SplashInitializer in app_blood_bank.dart`

--- lib/app_blood_bank.dart
+++ lib/app_blood_bank.dart
@@ -1,6 +1,6 @@
 import 'package:blood_bank/feature/localization/app_localizations.dart';
 import 'package:blood_bank/feature/localization/cubit/locale_cubit.dart';
-import 'package:blood_bank/feature/splash/Presentation/views/SplashInitializer.dart';
+import 'package:blood_bank/feature/splash/presentation/views/splash_initializer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
