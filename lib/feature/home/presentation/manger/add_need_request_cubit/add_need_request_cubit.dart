87332b7f2dc1bc7dd8c42c4beef8133b9c5fa5c2
engAhmedSami <xotornado@gmail.com>
2024-12-28 18:10:28 +0200

    `Added functionality to send notifications and fetch user data from Firestore in AddDonerRequestCubit, and updated NotificationService to send notifications to all users.`

--- lib/feature/home/presentation/manger/add_need_request_cubit/add_need_request_cubit.dart
+++ lib/feature/home/presentation/manger/add_need_request_cubit/add_need_request_cubit.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
-
 import 'package:blood_bank/feature/home/domain/entities/needer_request_entity.dart';
 import 'package:blood_bank/feature/home/domain/repos/needer_repo.dart';
 import 'package:blood_bank/feature/notification/notification_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 part 'add_need_request_state.dart';
 
 class AddNeederRequestCubit extends Cubit<AddNeedRequestState> {
