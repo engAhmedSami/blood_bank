872138202d4194a47161eba712e2d0683dc90e1c
engAhmedSami <xotornado@gmail.com>
2025-01-03 18:31:08 +0200

    `Removed unused imports and code, updated UI elements`

--- lib/feature/home/presentation/views/widget/home/requestes_for_donation.dart
+++ lib/feature/home/presentation/views/widget/home/requestes_for_donation.dart
@@ -1,232 +1,7 @@
-// import 'package:blood_bank/core/helper_function/get_user.dart';
-// import 'package:blood_bank/core/utils/app_colors.dart';
-// import 'package:blood_bank/core/utils/app_text_style.dart';
-// import 'package:blood_bank/core/utils/assets_images.dart';
-// import 'package:blood_bank/core/widget/coustom_circular_progress_indicator.dart';
-// import 'package:blood_bank/feature/auth/data/models/user_model.dart';
-// import 'package:blood_bank/feature/home/presentation/views/widget/see_all.dart';
-// import 'package:blood_bank/feature/localization/app_localizations.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
-// import 'package:top_snackbar_flutter/custom_snack_bar.dart';
-// import 'package:top_snackbar_flutter/top_snack_bar.dart';
-
-// class RequestsForDonation extends StatelessWidget {
-//   const RequestsForDonation({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final User? user = FirebaseAuth.instance.currentUser;
-//     return StreamBuilder(
-//       stream: FirebaseFirestore.instance.collection('donerRequest').snapshots(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return const Center(child: CoustomCircularProgressIndicator());
-//         }
-
-//         if (snapshot.hasError) {
-//           return Center(child: Text('error: ${snapshot.error}'));
-//         }
-
-//         final requests = snapshot.data?.docs ?? [];
-
-//         final reversedRequests = requests.reversed.toList();
-
-//         if (reversedRequests.isEmpty) {
-//           return Center(
-//               child: Text('no_donation_requests_available'.tr(context)));
-//         }
-
-//         return Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.symmetric(
-//                 horizontal: 20.0,
-//               ),
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   Text(
-//                     'donation_request'.tr(context),
-//                     style: TextStyles.semiBold16,
-//                   ),
-//                   SeeAll(requests: reversedRequests),
-//                 ],
-//               ),
-//             ),
-//             SizedBox(
-//               height: 350,
-//               child: ListView.builder(
-//                 itemCount:
-//                     reversedRequests.length >= 4 ? 4 : reversedRequests.length,
-//                 itemBuilder: (context, index) {
-//                   final request = reversedRequests[index].data();
-
-//                   return Padding(
-//                     padding: EdgeInsets.symmetric(
-//                       horizontal: 12,
-//                       vertical: 8,
-//                     ),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.white,
-//                         borderRadius: BorderRadius.circular(10),
-//                         boxShadow: [
-//                           BoxShadow(
-//                             color: Colors.black.withValues(
-//                               alpha: 0.4,
-//                             ),
-//                             blurRadius: 5,
-//                             offset: const Offset(0, 6),
-//                           ),
-//                         ],
-//                       ),
-//                       child: GestureDetector(
-//                         onTap: () {
-//                           // showTopSnackBar(
-//                           //   displayDuration: const Duration(seconds: 3),
-//                           //   Overlay.of(context),
-//                           //   CustomSnackBar.info(
-//                           //     backgroundColor: AppColors.backgroundColor,
-//                           //     textStyle: const TextStyle(
-//                           //       fontWeight: FontWeight.normal,
-//                           //       color: Colors.white,
-//                           //     ),
-//                           //     maxLines: 3,
-//                           //     textAlign: TextAlign.center,
-//                           //     message: request['name'] ?? 'no_name'.tr(context),
-//                           //   ),
-//                           // );
-//                           showTopSnackBar(
-//                             displayDuration: const Duration(seconds: 3),
-//                             Overlay.of(context),
-//                             CustomSnackBar.info(
-//                               backgroundColor: AppColors.backgroundColor,
-//                               textStyle: const TextStyle(
-//                                 fontWeight: FontWeight.normal,
-//                                 color: Colors.white,
-//                               ),
-//                               maxLines: 3,
-//                               textAlign: TextAlign.center,
-//                               message: 'donationDetails'.trWithParams(context,
-//                                   {'name': request['name'] ?? 'Unknown'}),
-//                             ),
-//                           );
-//                         },
-//                         child: ListTile(
-//                           leading: StreamBuilder<UserModel>(
-//                             stream: getUserStream(),
-//                             builder: (context, snapshot) {
-//                               if (snapshot.connectionState ==
-//                                   ConnectionState.waiting) {
-//                                 return const Center(
-//                                     child: CoustomCircularProgressIndicator());
-//                               }
-
-//                               if (snapshot.hasError) {
-//                                 return Center(
-//                                     child: Text('error: ${snapshot.error}'
-//                                         .tr(context)));
-//                               }
-
-//                               if (!snapshot.hasData) {
-//                                 return Center(
-//                                     child: Text(
-//                                         'no_user_data_available'.tr(context)));
-//                               }
-
-//                               final user = snapshot.data!;
-
-//                               return CircleAvatar(
-//                                 radius: 25,
-//                                 backgroundImage:
-//                                     NetworkImage(user.photoUrl ?? ''),
-//                               );
-//                             },
-//                           ),
-//                           title: Text(
-//                             request['name'] ?? 'no_name'.tr(context),
-//                             style: const TextStyle(
-//                                 fontSize: 16, fontWeight: FontWeight.w600),
-//                           ),
-//                           subtitle: Row(
-//                             children: [
-//                               const Icon(Icons.location_on,
-//                                   size: 16, color: Colors.grey),
-//                               Text(
-//                                 request['hospitalName'] ??
-//                                     'unknown_hospital'.tr(context),
-//                                 style: const TextStyle(
-//                                     fontSize: 12, color: Colors.grey),
-//                               ),
-//                             ],
-//                           ),
-//                           trailing: Row(
-//                             mainAxisSize: MainAxisSize.min,
-//                             children: [
-//                               Column(
-//                                 mainAxisAlignment: MainAxisAlignment.center,
-//                                 children: [
-//                                   Stack(
-//                                     alignment: Alignment.center,
-//                                     children: [
-//                                       SvgPicture.asset(
-//                                         Assets.imagesBlooddrop,
-//                                         height: 35,
-//                                       ),
-//                                       Text(
-//                                         '${request['bloodType'] ?? 'N/A'}',
-//                                         style: TextStyles.semiBold11.copyWith(
-//                                           color: Colors.white,
-//                                         ),
-//                                         textAlign: TextAlign.center,
-//                                       ),
-//                                     ],
-//                                   ),
-//                                 ],
-//                               ),
-//                               const SizedBox(width: 12),
-//                               Column(
-//                                 mainAxisAlignment: MainAxisAlignment.center,
-//                                 children: [
-//                                   Container(
-//                                     padding: const EdgeInsets.symmetric(
-//                                         vertical: 2, horizontal: 6),
-//                                     decoration: BoxDecoration(
-//                                         color: const Color(0xff598158),
-//                                         borderRadius: BorderRadius.circular(8)),
-//                                     child: Text(
-//                                       '${request['distance'] ?? '0'} km',
-//                                       style: TextStyles.semiBold12
-//                                           .copyWith(color: Colors.white),
-//                                     ),
-//                                   ),
-//                                 ],
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   );
-//                 },
-//               ),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-import 'package:blood_bank/core/helper_function/get_user.dart';
 import 'package:blood_bank/core/utils/app_colors.dart';
 import 'package:blood_bank/core/utils/app_text_style.dart';
 import 'package:blood_bank/core/utils/assets_images.dart';
 import 'package:blood_bank/core/widget/coustom_circular_progress_indicator.dart';
-import 'package:blood_bank/feature/auth/data/models/user_model.dart';
 import 'package:blood_bank/feature/home/presentation/views/widget/see_all.dart';
 import 'package:blood_bank/feature/localization/app_localizations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -287,7 +62,7 @@ class RequestsForDonation extends StatelessWidget {
                   final request = reversedRequests[index].data();
 
                   return Padding(
-                    padding: EdgeInsets.symmetric(
+                    padding: const EdgeInsets.symmetric(
                       horizontal: 12,
                       vertical: 8,
                     ),
@@ -297,9 +72,7 @@ class RequestsForDonation extends StatelessWidget {
                         borderRadius: BorderRadius.circular(10),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withValues(
-                              alpha: 0.4,
-                            ),
+                            color: Colors.black.withValues(alpha: 0.3),
                             blurRadius: 5,
                             offset: const Offset(0, 6),
                           ),
@@ -325,10 +98,13 @@ class RequestsForDonation extends StatelessWidget {
                         },
                         child: ListTile(
                           leading: SizedBox(
-                            width: 50, // تحديد عرض ثابت للـ leading
-                            height: 50, // تحديد ارتفاع ثابت للـ leading
-                            child: StreamBuilder<UserModel>(
-                              stream: getUserStream(),
+                            width: 50,
+                            height: 50,
+                            child: FutureBuilder<DocumentSnapshot>(
+                              future: FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(request['uId'])
+                                  .get(),
                               builder: (context, snapshot) {
                                 if (snapshot.connectionState ==
                                     ConnectionState.waiting) {
@@ -343,18 +119,30 @@ class RequestsForDonation extends StatelessWidget {
                                           .tr(context)));
                                 }
 
-                                if (!snapshot.hasData) {
-                                  return Center(
-                                      child: Text('no_user_data_available'
-                                          .tr(context)));
+                                if (!snapshot.hasData ||
+                                    !snapshot.data!.exists) {
+                                  return CircleAvatar(
+                                    radius: 25,
+                                    backgroundColor: Colors.grey,
+                                    child:
+                                        Icon(Icons.person, color: Colors.white),
+                                  );
                                 }
 
-                                final user = snapshot.data!;
+                                final userData = snapshot.data!.data()
+                                    as Map<String, dynamic>;
+                                final photoUrl = userData['photoUrl'];
 
                                 return CircleAvatar(
                                   radius: 25,
                                   backgroundImage:
-                                      NetworkImage(user.photoUrl ?? ''),
+                                      photoUrl != null && photoUrl.isNotEmpty
+                                          ? NetworkImage(photoUrl)
+                                          : null,
+                                  backgroundColor: Colors.grey,
+                                  child: photoUrl == null || photoUrl.isEmpty
+                                      ? Icon(Icons.person, color: Colors.white)
+                                      : null,
                                 );
                               },
                             ),
