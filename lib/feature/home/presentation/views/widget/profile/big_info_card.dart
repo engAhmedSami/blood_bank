dd752757d85b8c8e94200e18a25a6764872128e3
Basel Embaby <engbasel2025@gmail.com>
2025-01-11 13:11:30 +0200

    `Added width variable and SizedBox widgets to BigInfoCard widget`

--- lib/feature/home/presentation/views/widget/profile/big_info_card.dart
+++ lib/feature/home/presentation/views/widget/profile/big_info_card.dart
@@ -1,11 +1,10 @@
-import 'package:blood_bank/core/utils/app_text_style.dart';
 import 'package:blood_bank/core/utils/assets_images.dart';
 import 'package:blood_bank/core/widget/coustom_circular_progress_indicator.dart';
 import 'package:blood_bank/core/widget/coustom_dialog.dart';
+import 'package:blood_bank/feature/home/presentation/views/widget/profile/InfoColumn.dart';
 import 'package:blood_bank/feature/localization/app_localizations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -40,6 +39,7 @@ class BigInfoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     final currentUserUid = FirebaseAuth.instance.currentUser?.uid;
     if (currentUserUid == null) {
       return const Center(child: Text('No user is logged in'));
@@ -112,8 +112,11 @@ class BigInfoCard extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                SizedBox(width: width * 0.05),
                 InfoColumn(title: savedLives, image: Assets.imagesLifesaved),
+                SizedBox(width: width * 0.05),
                 InfoColumn(title: bloodGroup, image: Assets.imagesBlood),
+                SizedBox(width: width * 0.05),
                 InfoColumn(
                   title: isTodayDonationDay
                       ? 'Today is your donation day'.tr(context)
@@ -121,6 +124,7 @@ class BigInfoCard extends StatelessWidget {
                   image: Assets.imagesNextdonation,
                   isTodayDonationDay: isTodayDonationDay,
                 ),
+                SizedBox(width: width * 0.05),
               ],
             ),
           ),
@@ -166,36 +170,6 @@ class BigInfoCard extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class InfoColumn extends StatelessWidget {
-  final String title;
-  final String image;
-  final bool isTodayDonationDay;
-
-  const InfoColumn({
-    super.key,
-    required this.title,
-    required this.image,
-    this.isTodayDonationDay = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SvgPicture.asset(image, height: 30),
-        const SizedBox(height: 8),
-        Text(
-          title,
-          textAlign: TextAlign.center,
-          style: isTodayDonationDay
-              ? TextStyles.semiBold13.copyWith(color: Colors.red)
-              : TextStyles.semiBold13,
-        ),
-      ],
     );
   }
 }
